d8daa519d215445ed9222809d92869a54484a99b
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2022-02-07 07:24:53 -0300

    Importando fonte opensans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
+//* Importando fonte para ser usada
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'Open Sans',
+          ...defaultTheme.fontFamily.sans,
+        ]
+      }
+    },
   },
   plugins: [],
 }
